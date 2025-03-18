c69a52fec9d8c8e20c52892c3672da2b7ab0dcd4
Manuel Chave <manuelmariomanuel199@gmail.com>
2025-03-18 18:26:07 +0200

    Adding styles and js content to the site

---
+++ scripts/getDates.js
@@ -0,0 +1,4 @@
+let date = new Date()
+yearCopright = date.getFullYear()
+document.getElementById("year").innerHTML = "© "+ yearCopright
+document.getElementById("lastModified").innerHTML = "Last Modification:" + date
