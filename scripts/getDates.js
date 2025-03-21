557a0a3354b163c69c1d10329ea2d1311fe4950c
Manuel Chave <manuelmariomanuel199@gmail.com>
2025-03-21 22:30:58 +0200

    Adding media query content and fixed some css errors

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,4 +1,4 @@
 let date = new Date()
 yearCopright = date.getFullYear()
 document.getElementById("year").innerHTML = "© "+ yearCopright
-document.getElementById("lastModified").innerHTML = "Last Modification:" + date
+document.getElementById("lastModified").innerHTML = "Last Modification: " + document.lastModified
