437df1ed9eb0f0f7bab812d15430c39f20b3b8ea
Manuel Chave <manuelmariomanuel199@gmail.com>
2025-03-30 23:47:36 +0200

    Added hero and map content, fixed hamburguer problem

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,3 +1,4 @@
+
 let date = new Date()
 yearCopright = date.getFullYear()
 document.getElementById("year").innerHTML = "© "+ yearCopright
@@ -5,12 +6,10 @@ document.getElementById("lastModified").innerHTML = "Last Modification: " + docu
 
 const btn_humburguer = document.querySelector(".hamburger")
 const nav_active_element = document.querySelector("#animateme")
-
-
 const mode_element = document.querySelector(".mode")
 const main_element = document.querySelector("main")
-
-
+const list_element = document.querySelector(".list")
+const heroOverlay = document.querySelector(".hero-overlay");
 
 btn_humburguer.addEventListener('click', () => {
     nav_active_element.classList.toggle('open');
@@ -18,14 +17,36 @@ btn_humburguer.addEventListener('click', () => {
 
 });
 
-mode_element-addEventListener('click',() => {
-    if (mode_element.textContent.includes("🌞"))
-         {
-		main_element.style.background = "#242625";
-		
+mode_element.addEventListener('click', () => {
+    if (mode_element.textContent.includes("🌞")){
+		main_element.style.background = "#242625";			
+		list_element.style.color ="#F2F2F2";
+		heroOverlay.style.backgroundColor = "#000000";
 		mode_element.textContent = "🌘";
 	} else {
-		main_element.style.background = "#F2F2F2";
+		main_element.style.background = "";   
+		list_element.style.color =" black";
+		heroOverlay.style.backgroundColor = "#0b0c0d";
 		mode_element.textContent = "🌞";
 	}
 })
+
+setupVisitCounter()
+
+
+function setupVisitCounter() {
+    const visitDisplay = document.getElementById("visit-counter");
+    
+    if (visitDisplay) {
+       
+        let numVisits = Number(localStorage.getItem("visits")) || 0;
+
+        numVisits++;
+
+        visitDisplay.textContent = `Page visits: ${numVisits}`;
+ 
+        localStorage.setItem("visits", numVisits);
+    }
+}
+	
+
